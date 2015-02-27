c35942abe1f5467271e8bbc48609758db8822e5c
Michael Gunnulfsen <michael.gunnulfsen@finn.no>
2015-02-27 16:36:49 +0100

    better filename handling

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
-var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var qt = require('quickthumb');
+var morgan = require('morgan');
+var logger = morgan('combined');
+
 
 var routes = require('./routes/index');
 
@@ -13,13 +15,14 @@ var app = express();
 // Use quickthumb
 app.use(qt.static(__dirname + '/uploads'));
 
+app.use(logger);
+
 // view engine setup
 // app.set('views', path.join(__dirname, 'views'));
 // app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
--- routes/index.js
+++ routes/index.js
@@ -3,28 +3,33 @@ var router = express.Router();
 var formidable = require('formidable');
 var util = require('util');
 var fs = require('fs-extra');
-var morgan = require('morgan');
-var logger = morgan('combined');
 
 
 
-function _onFormEnd(fields, files) {
+function _onFormEnd(res, fields, files) {
 
     /* Temporary location of our uploaded file */
     var temp_path = this.openedFiles[0].path;
 
     /* The file name of the uploaded file */
-    var file_name = this.openedFiles[0].name;
+    var file_name = this.filename || this.openedFiles[0].name;
 
     /* Location where we want to copy the uploaded file */
     var new_location = 'uploads/';
 
-    fs.copy(temp_path, new_location + file_name, function(err) {
+    var new_file_path = new_location + file_name;
+
+    fs.copy(temp_path, new_file_path, function(err) {
         if (err) {
             console.error(err);
+            res.status(500).send({err : err});
+
         } else {
-            console.log("success!")
+            console.log("success! " + new_file_path);
+            res.send({filename : file_name});
         }
+
+
     });
 }
 
@@ -42,13 +47,13 @@ function post(req, res) {
 
     form.parse(req, function onParse(err, fields, files) {
 
-        res.writeHead(200, {'content-type': 'text/plain'});
-        res.write('received upload:\n\n');
-        res.end(util.inspect({fields: fields, files: files}));
+        console.log('incoming file: ' + JSON.stringify(fields) + ', ' + JSON.stringify(files));
 
-    });
+        this.filename = fields.filename;
 
-    form.on('end', _onFormEnd);
+    }.bind(form));
+
+    form.on('end', _onFormEnd.bind(form, res));
 };
 
 
